607b8df393aa4c32e0e23b0127178648dcef8f89
aprilwldnjs <aprilwldnjs@gmail.com>
2024-05-16 18:09:13 -0400

    chore: Customize gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
-  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
-  ogImage: `/og-image.png`, // Path to your in the 'static' folder
+  title: `JiyaJiwon`,
+  description: `Classic Coding Log`,
+  language: `en`, // `ko`, `en` => currently support versions for Korean and English
+  siteUrl: `https://jiyajiwon.github.io/`,
+  ogImage: `static/backgroundImg.jpeg`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: ``, // `jiyajiwon/jiyajiwon.github.io`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `Jiwon Ryu`,
     bio: {
-      role: `개발자`,
+      role: `Embeded Developer`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: ``, // `https://github.com/jiyajiwon`,
+      linkedIn: ``, // `https://www.linkedin.com/in/jiwonryu31/`,
+      email: ``, // `jiwonryu31@gmail.com`,
     },
   },
 
